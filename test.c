24feed8385bdcffe03adfe86494b27d14be8179e
imcheong <imcheong001@gmail.com>
2019-06-07 01:37:59 +0900

    char배열 순서 반대로 / 대소문자 치환 출력 함수

--- test.c
+++ test.c
@@ -1,6 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <string.h>
+#include <ctype.h>
 #include <stdio.h>
 
-int main(){
-    printf("testing");
-    return 0;
+void main(){
+    char str[50];
+    char resultStr[50];
+    int k=0;
+    printf("문자열을 대소문자 구분하여 입력 : ");
+    scanf("%s",str);
+    printf("-------------------------------------------------\n");
+    int length = strlen(str);
+    for(int i=length-1; i>=0; i--){
+       if (isupper(str[i])) {
+            resultStr[k++] = tolower(str[i]);
+        } else if (islower(str[i])) {
+            resultStr[k++] = toupper(str[i]);
+        }
+    }
+    resultStr[k] = '\0';
+    printf("변환된 결과물 출력 ==> %s\n", resultStr);
+    return;
 }
